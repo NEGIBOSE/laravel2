4f2f2edd44f038dab2a08a2f5e4621344b89fa90
NEGIBOSE <ryo0827ngsh@icloud.com>
2024-02-26 14:35:27 +0900

    p43,05

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/sample', [\App\Http\Controllers\Sample\IndexController::class, 'show']);
